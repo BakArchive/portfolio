6937252e2c9ed519d0af78e2902f94c6c8dc661d
MuenYu <parasomnia97@gmail.com>
2024-03-29 20:51:35 +1000

    GithubCalendar optimization

--- src/components/GithubCalendar.jsx
+++ src/components/GithubCalendar.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import Loading from "@/components/Loading";
+
 function GithubCalendar({ username, className }) {
+  const [load, setLoad] = useState(false);
+
   return (
     <figure className={`w-full overflow-auto ${className}`}>
+      {!load && <Loading className="h-40" />}
       <img
         src={`https://ghchart.rshah.org/${username}`}
         alt={`${username}'s Github chart`}
         className="w-full min-w-[1000px]"
+        onLoad={() => {
+          setLoad(true);
+        }}
       />
     </figure>
   );
