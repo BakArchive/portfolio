3634e039e12897bdc367ffc769494494835305b8
MuenYu <parasomnia97@gmail.com>
2024-12-07 23:39:48 +1000

    change resume to skill

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,18 +1,12 @@
-// "use client";
 import Link from "next/link";
-
-const downloadResume = () => {
-  alert("Download Resume");
-};
 
 export default function Nav() {
   return (
     <nav className="shadow bg-base-100 flex flex-wrap justify-evenly py-4 md:rounded-full w-full md:w-[48rem]">
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
+      <Link href="/skills">Skills</Link>
       <Link href="/projects">Projects</Link>
-      <Link href="/resume">Resume</Link>
-      {/* <a onClick={downloadResume}>Resume</a> */}
     </nav>
   );
 }
---
+++ app/skills/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Skills | Portfolio",
+    description: "Skills page of my portfolio"
+}
+
+export default function About() {
+    return <p>Skills</p>;
+}
