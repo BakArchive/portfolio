a677e2983df3dead77b6f1c17b149c5d71605bc1
MuenYu <parasomnia97@gmail.com>
2024-12-08 12:34:42 +1000

    new footer

--- app/components/FootNote.tsx
+++ app/components/FootNote.tsx
@@ -1,7 +1,31 @@
+import {
+  IconArchive,
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandLeetcode,
+} from "@tabler/icons-react";
+
 const year: number = new Date().getFullYear();
 
 export default function FootNote() {
   return (
-      <p className="text-center">Crafted by Muen Yu | ©{year} All rights reserved</p>
+    <div className="flex justify-evenly gap-6">
+      <p>Designed & Crafted by Muen Yu</p>
+      <p>CopyRight ©{year} All rights reserved</p>
+      <div className="flex gap-5">
+        <a href="" target="_blank">
+          <IconArchive stroke={1} />
+        </a>
+        <a href="" target="_blank">
+          <IconBrandLinkedin stroke={1} />
+        </a>
+        <a href="" target="_blank">
+          <IconBrandGithub stroke={1} />
+        </a>
+        <a href="" target="_blank">
+          <IconBrandLeetcode stroke={1} />
+        </a>
+      </div>
+    </div>
   );
 }
