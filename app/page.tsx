9d4f47486e5f29ec65a460a4761495c14e9f79a1
MuenYu <parasomnia97@gmail.com>
2024-12-08 18:56:26 +1000

    move words as variable

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,21 @@ export const metadata: Metadata = {
   description: "Home page of my portfolio",
 };
 
+const words: string[] = [
+  "a software developer",
+  "a QUT graduate",
+  "an open-source contributor",
+];
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center md:flex-row h-full">
       <div className="flex flex-col text-left md:w-[32rem] gap-5">
         <h1 className="text-6xl">Hello,</h1>
         <h1 className="text-6xl">I'm Muen Yu</h1>
-        <h2 className="text-4xl">As <TypedText words={["a software developer", "a QUT graduate", "an open-source contributor"]} /></h2>
+        <h2 className="text-4xl">
+          As <TypedText words={words} />
+        </h2>
       </div>
       <LottieAnimation animation={lottieJson} className="w-96" />
     </div>
