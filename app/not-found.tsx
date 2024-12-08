605f983398460adc5f6190078b962cdf32e3a405
MuenYu <parasomnia97@gmail.com>
2024-12-09 00:34:34 +1000

    apply animation in 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,19 @@
 import { Metadata } from "next";
+import LottieAnimation from "./components/LottieAnimation";
 
 export const metadata: Metadata = {
-    title: "404 Not Found",
-    description: "The page does not exist"
-}
+  title: "404 Not Found",
+  description: "The page does not exist",
+};
 
 export default function NotFound() {
-    return (
-        <div className="h-full flex flex-col gap-4 justify-center md:items-center">
-            <h1 className="text-6xl">Not Found</h1>
-            <h2 className="text-4xl">The page does not exist</h2>
-        </div>
-    )
+  return (
+    <div className="h-full flex flex-col gap-4 justify-center items-center">
+      <LottieAnimation
+        animationPath="/animation/notfound.json"
+        className="w-60 h-60"
+      />
+      <h1 className="text-6xl">Not Found</h1>
+    </div>
+  );
 }
