f263df5032dfc7fc0027eaddfc3a5af6a83980db
MuenYu <parasomnia97@gmail.com>
2024-12-20 12:47:35 +1000

    scroll to change page

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,30 +1,62 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
+import { useEffect } from "react";
+import _ from "lodash";
 
-const navItems: Record<string, string> = {
-  "/": "Home",
-  "/about": "About",
-  "/skills": "Skills",
-  "/projects": "Projects",
-};
+const navItems: { path: string; name: string }[] = [
+  { path: "/", name: "Home" },
+  { path: "/about", name: "About" },
+  { path: "/skills", name: "Skills" },
+  { path: "/projects", name: "Projects" },
+];
 
-export default function Nav({className}:{className?:string}) {
-  const pathname = usePathname(); 
-  const navItemClass = "text-slate-500 rounded-full px-3 py-1";
-  const active = "bg-base-200";
+const navItemClass = "text-slate-500 rounded-full px-3 py-1";
+const active = "bg-base-200";
+
+export default function Nav({ className }: { className?: string }) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleScroll = _.debounce((isUp: boolean, pathname: string) => {
+    const isTop = window.scrollY === 0;
+    const isBottom =
+      window.scrollY + window.innerHeight >=
+      document.documentElement.scrollHeight;
+
+    const index = navItems.findIndex((item) => item.path === pathname);
+    if (isTop && isUp && index > 0) {
+      router.push(navItems[index - 1].path);
+    } else if (isBottom && !isUp && index + 1 < navItems.length) {
+      router.push(navItems[index + 1].path);
+    }
+  }, 300);
+
+  useEffect(() => {
+    const handleWheel = (event: any) => {
+      const isUp = event.deltaY > 0 ? false : true;
+      handleScroll(isUp, pathname);
+    };
+
+    window.addEventListener("wheel", handleWheel);
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, [pathname]);
 
   return (
-    <nav className={`shadow bg-base-100 flex flex-wrap justify-evenly py-2 md:rounded-full ${className || ""}`}>
-      {Object.entries(navItems).map(([path, label]) => (
+    <nav
+      className={`shadow bg-base-100 flex flex-wrap justify-evenly py-2 md:rounded-full ${className}`}
+    >
+      {navItems.map((item, idx) => (
         <Link
-          href={path}
-          key={path}
-          className={`${
-            pathname === path ? `${navItemClass} ${active}`:navItemClass
+          href={item.path}
+          key={idx}
+          className={`${navItemClass} ${
+            pathname === item.path ? `${active}` : ""
           }`}
         >
-          {label}
+          {item.name}
         </Link>
       ))}
     </nav>
